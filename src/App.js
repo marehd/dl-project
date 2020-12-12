d0e1bec597a423a4d2becbd0d17476d5a6851c6e
Marlena Rehder <rehder99@cs.washington.edu>
2020-12-12 03:27:11 -0800

    Current base version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      startingText: "Default text",
+      textType: "default"
+      
+    };
+
+    this.generateGood = this.generateGood.bind(this);
+    this.generateBad = this.generateBad.bind(this);
+  }
+
+  generateGood() {
+    this.setState({startingText: "hello this is very long", textType: "default"}, () => this.setState({startingText: "Good", textType: "generated-post"}));
+  }
+
+  generateBad() {
+    this.setState({startingText: "", textType: "default"}, () => this.setState({startingText: "Bad", textType: "generated-post"}));
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className = "box">
+          <section id="generator-title">
+            <h2>Here is a title.</h2>
+          <div id="button">
+            <button onClick={this.generateGood}>+ Generate good</button>
+            <button onClick={this.generateBad}>+ Generate bad</button>
+          </div>
+          </section>
+          <div>
+            <p id={this.state.textType}>{this.state.startingText}</p>
+          </div>
+        </div>
+        <div className = "box">
+          <h2>Here is a title.</h2>
+          <p>Hello here is some text</p>
+        </div>
+        <div className = "box">
+          <h2>Here is a title.</h2>
+          <p>Hello here is some text</p>
+        </div>
+        <div className = "box">
+          <h2>Here is a title.</h2>
+          <p>Hello here is some text</p>
+        </div>
+        <div className = "box">
+          <h2>Here is a title.</h2>
+          <p>Hello here is some text</p>
+        </div>
+        <div className = "box">
+          <h2>Here is a title.</h2>
+          <p>Hello here is some text</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
